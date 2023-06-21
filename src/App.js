d59562e816958cd6944bb0a3df5034a4213fb696
godsonCodes <ofemi_001@yahoo.com>
2023-06-21 05:50:09 +0100

    created comonents and added open graph protocol

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
+import Specials from './components/specials/Specials'
+import Testimonials from './components/testimonials/Testimonials'
 
 function App() {
-  return <div className='App'>Homepage</div>
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Specials />
+      <Testimonials />
+      <Footer />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <img src='../images/Logo .svg' alt='' />
+      <nav className='navbar'>
+        <ul>
+          <li>
+            <a href='#home'>Home</a>
+          </li>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#menu'>Menu</a>
+          </li>
+          <li>
+            <a href='#'>Reservation</a>
+          </li>
+          <li>
+            <a href='#'>Order Online</a>
+          </li>
+          <li>
+            <a href='#'>Login</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
